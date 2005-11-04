8acc14e0ecda520d9c44314ef6cc45bc6886708a
gha <gha>
2005-11-04 22:31:08 +0000

    pequenas altera?oes no material

--- engine/src/modules/material/material.h
+++ engine/src/modules/material/material.h
@@ -4,14 +4,14 @@
 #include "../textures/texture.h"
 
 /* classe totalmente experimental ainda
- * eh apenas um esbo?o, ainda vai mudar muito */
+*/
 
 class material {
 public:
 
-	bool isName(std::string &text){if (strcmp(text.c_str(), name.c_str()) == 0) return true; return false;};
 	std::string name;
-	float faceColor[4];
+	float color[4];
+	float secundaryColor[4];
 	float power;
 	float specularColor[4], emissiveColor[4];
 
