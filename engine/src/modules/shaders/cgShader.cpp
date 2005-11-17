4921e34d327af14d434d9c6538e410f3acc6b3b0
gha <gha>
2005-11-17 01:06:41 +0000

    classe cgshader quase completa

--- engine/src/modules/shaders/cgShader.cpp
+++ engine/src/modules/shaders/cgShader.cpp
@@ -1,20 +1,60 @@
 
 #include "cgShader.h"
 
-bool cgShader::initialize(char* vertexfile, CGprofile vertex, char * fragmentfile, CGprofile fragment){
+void cgShader::kill(){
+	
+	if (hasvertex)
+		cgDestroyProgram(vertexprogram);
+		
+	if (hasfrag)
+		cgDestroyProgram(fragmentprogram);
+	
+}
+
+void cgShader::bind(){
+
+	if (hasvertex){
+		cgGLEnableProfile(vertexprofile);
+		cgGLBindProgram(vertexprogram);
+	}
+	
+	if (hasfrag){
+		cgGLEnableProfile(fragmentprofile);
+		cgGLBindProgram(fragmentprogram);
+	}
+	
+}
+
+void cgShader::unbind(){
+
+	if(hasvertex){
+		cgGLDisableProfile(vertexprofile);
+	}
+	
+	if (hasfrag){
+		cgGLDisableProfile(fragmentprofile);
+	}
+	
+}
+
+bool cgShader::initialize(char* vertexfile, int vertex, char * fragmentfile, int fragment){
 
 	context = cgCreateContext();
-	if (cgGLIsProfileSupported(vertex))
-		vertexprofile = vertex;
-	else
+	
+	if (vertex == PROFILE_ARBVP1){
+		vertexprofile = CG_PROFILE_ARBVP1;
+	}else{
 		return false;
+	}
 		
-	if (cgGLIsProfileSupported(fragment))
-		fragmentprofile = fragment;
-	else
-		return false;	
+	if (vertex == PROFILE_ARBFP1){
+		fragmentprofile = CG_PROFILE_ARBFP1;
+	}else{
+		return false;
+	}	
 	
 	if (vertexfile){
+		hasvertex = true;
 		vertexprogram = cgCreateProgramFromFile(context, CG_SOURCE, vertexfile, vertexprofile, NULL, NULL);
 		if (!cgIsProgramCompiled(vertexprogram))
 			cgCompileProgram(vertexprogram);
@@ -24,6 +64,7 @@ bool cgShader::initialize(char* vertexfile, CGprofile vertex, char * fragmentfil
 	}
 	
 	if (fragmentfile){
+		hasfrag = true;
 		fragmentprogram = cgCreateProgramFromFile(context, CG_SOURCE, fragmentfile, fragmentprofile, NULL, NULL);
 		if (!cgIsProgramCompiled(fragmentprogram))
 			cgCompileProgram(fragmentprogram);
@@ -32,5 +73,5 @@ bool cgShader::initialize(char* vertexfile, CGprofile vertex, char * fragmentfil
 		cgGLLoadProgram(fragmentprogram);
 	}
 	
-	
+	return true;
 }
--- engine/src/modules/shaders/cgShader.h
+++ engine/src/modules/shaders/cgShader.h
@@ -6,21 +6,33 @@
 /* interface basica para shader em Cg
  */
 
+//TODO adicionar mais profiles na enum
+//TODO verificar o caso dos profiles e kill 
+//TODO verificar o unbind() para parar o program
+
+enum {
+	PROFILE_ARBVP1 = 1,
+	PROFILE_ARBFP1
+	
+};
+
 class cgShader{
 	
 public:
 
-	cgShader();
-	virtual ~cgShader();
+	cgShader(char *vertexfile, int vertex, char * fragmentfile, int fragment) { initialize(vertexfile, vertex, fragmentfile , fragment); };
+	virtual ~cgShader() { kill(); };
 	
-	bool initialize(char *vertexfile, CGprofile vertex, char * fragmentfile, CGprofile fragment );
-	virtual void bind() = 0;
-	virtual void unbind() =0;
-	virtual void setParameters() = 0;
+	bool initialize(char *vertexfile, int vertex, char * fragmentfile, int fragment );
+	void kill();
+	void bind();
+	void unbind();
+	virtual void setInitialParameters() = 0;
+	virtual void setLoopParameters() = 0;
 
 
 private:
-	
+	bool hasfrag, hasvertex;
 	CGcontext context;
 	CGprogram vertexprogram, fragmentprogram;
 	CGprofile vertexprofile, fragmentprofile;
