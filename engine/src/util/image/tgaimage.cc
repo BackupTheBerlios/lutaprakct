01d1adc6e685f3d904f5429e7b8eb85249acac14
gha <gha>
2006-07-02 17:49:35 +0000

    retirado alguns cout inuteis.

--- engine/src/util/image/tgaimage.cc
+++ engine/src/util/image/tgaimage.cc
@@ -22,7 +22,6 @@ bool tgaimage::processHeader(){
                       //               11 = RLE greyscale
                       //               32 & 33 Other compression, indexed
 
-std::cout << "encode " << imagedata[2] << std::endl;
 	if(encode > 11)  
 		return false;
 
@@ -98,7 +97,6 @@ bool tgaimage::load(const char *filename, int loadingflags){
 
 		case 2: // Raw RGB
    		{
-   				std::cout << "lendo tga" << std::endl;
 			if((imagesize+18+imagedata[0])>filesize){
 				std::cout << "invalid file size" << std::endl;
 				return false;
@@ -119,7 +117,7 @@ bool tgaimage::load(const char *filename, int loadingflags){
       }
 
 		default:
-			std::cout << "sei nao " << encode << std::endl;
+			std::cout << "tga invalido " << encode << std::endl;
 			return false;
     }
 
@@ -139,6 +137,52 @@ bool tgaimage::load(const char *filename, int loadingflags){
 	height = img->h;
 	//bpp = img->bpp;
 */
+/*GLubyte TGAheader[12] = { 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+	GLubyte TGAcmp[12];
+	GLubyte header[6];
+	GLuint imagesize, temp, bytesPerPixel;
+std::cout << "lendo tga" << std::endl;
+	FILE *f = fopen (filename, "rb");
+	
+	if ((f == NULL)
+	    || (fread (TGAcmp, 1, sizeof (TGAcmp), f) != sizeof (TGAcmp))
+	    || (memcmp (TGAheader, TGAcmp, sizeof (TGAheader)) != 0)
+	    || (fread (header, 1, sizeof (header), f) != sizeof (header))){
+		fclose (f);
+		std::cout << "nao foi possivel ler a textura" << std::endl;
+		return false;
+	}
+	width = header[1] * 256 + header[0];
+	height = header[3] * 256 + header[2];
+	std::cout << "tamanho " <<width << std::endl;
+	bpp = header[4];
+	bytesPerPixel = bpp / 8;
+	imagesize = width * height * bytesPerPixel;
+
+	if ((width <= 0) || (height <= 0)
+	    || ((header[4] != 24) && (header[4] != 32))){
+		fclose (f);
+		return false;
+	}
+
+	imagedata = new GLubyte[imagesize];
+	if ((imagedata == NULL)
+	    || (fread (imagedata, 1, imagesize, f) != imagesize)){
+		delete imagedata;
+		fclose (f);
+		return false;
+	}
+	//swap no formato BGR do TGA pra RGB somente se
+	//nao for especificado o formato BGR ou BGRA
+//	if ( (format != GL_BGR_EXT) && (format != GL_BGRA_EXT) ){
+ 		for (GLuint i = 0; i < (imagesize); i += bytesPerPixel){
+			temp = imagedata[i];	//guarda o valor do B
+			imagedata[i] = imagedata[i + 2];	//B recebe o valor do R
+			imagedata[i + 2] = temp;	//R recebe o valor de B, ficando RGB
+		}
+//	}
+	fclose (f);
+	return true;*/
 	return true;
 	
 }
